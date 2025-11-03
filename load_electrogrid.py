3242ef2aa9e4e5c0d6e6d18d375cdf3be8d7d95a
jnuneznova <julian.nnova08@gmail.com>
2025-11-03 18:51:16 +0000

    electrogrid changes

--- load_electrogrid.py
+++ load_electrogrid.py
@@ -1,10 +1,68 @@
 # Configuration 
 
-PGHOST = "localhost"
+PGHOST = "dbm.fe.up.pt"
 PGPORT = 5433
 PGDATABASE = "fced01"
 PGUSER = "fced01"
 PGPASSWORD = "GROUP1"
-PGSCHEMA = "public"
+PGSCHEMA = "electrogrid"
 
 CSV_DIR = "./raw_data" 
+
+#Establishing database access
+
+import psycopg2
+
+conn = psycopg2.connect(
+        host=PGHOST,
+        port=PGPORT,
+        dbname=PGDATABASE,
+        user=PGUSER,
+        password=PGPASSWORD
+    )
+cursor = conn.cursor()
+
+TABLES = [
+    "service_orders",
+    "bills",
+    "connections",
+    "technician_skill",
+    "skills",
+    "technician",
+    "client",
+    "person",
+    "service_type",
+    "status",
+    "connection_type",
+    "region"
+]
+
+try:
+    # Connect to the database
+    conn = psycopg2.connect(
+        host=PGHOST,
+        port=PGPORT,
+        dbname=PGDATABASE,
+        user=PGUSER,
+        password=PGPASSWORD
+    )
+    cursor = conn.cursor()
+    print("Connected successfully!")
+
+    # Delete from each table
+    for table in TABLES:
+        cursor.execute(f"DELETE FROM {PGSCHEMA}.{table};")
+        print(f" Cleared table: {table}")
+
+    # Commit changes
+    conn.commit()
+    print(" All tables cleared successfully.")
+
+except Exception as e:
+    print("Error clearing tables:", e)
+    conn.rollback()
+
+
+#Read, clean, and transform the data
+
+#
